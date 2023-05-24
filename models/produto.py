f8b86a5f5a9ba58f52d85dfcf3e77e1f6ec7b06c
Leonardo Vieira <leonardo.vieira@livonsaude.com.br>
2023-05-24 20:21:16 -0300

    remocao parametro desnecessario no modelo de produto

--- models/produto.py
+++ models/produto.py
@@ -4,8 +4,9 @@ from odoo import fields, models
 class Produto(models.Model):
     _name = 'pedidos.produto'
     _description = 'Produto'
+    _rec_name = 'nome'
 
     nome = fields.Char('Nome do Produto', required=True)
     descricao = fields.Text('Descrição do Pedido')
-    preco = fields.Float('Preço do Produto', digits=2)
+    preco = fields.Float('Preço do Produto')
     pedido_id = fields.Many2one('pedidos.pedido', 'Pedido')
